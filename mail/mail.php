4b79b9efa4041cb743ee0c4b9199d1aea689dda6
Volodymyr Prykhodko <fliros@windowslive.com>
2024-05-29 11:07:23 +0200

    Mail client

--- mail/mail.php
+++ mail/mail.php
@@ -6,28 +6,28 @@
 
 // Choose here whether to use php mail() function or your SMTP server (recommended) to send the email.
 // Use 'smtp' for better reliability, or use 'phpmail' for quick + easy setup with lower reliability.
-$emailMethod                = 'smtp'; // REQUIRED value. Options: 'smtp' , 'phpmail'
+$emailMethod                = 'phpmail'; // REQUIRED value. Options: 'smtp' , 'phpmail'
 
 // Outgoing Server Settings - replace values on the right of the = sign with your own.
 // These 3 settings are only required if you choose 'smtp' for emailMethod above.
-$outgoingServerAddress      = 'mail.yourdomain.com'; // Consult your hosting provider.
-$outgoingServerPort         = '25';                  // Options: '587' , '25' - Consult your hosting provider.
-$outgoingServerSecurity     = 'tls';                 // Options: 'ssl' , 'tls' , null - Consult your hosting provider.
+$outgoingServerAddress      = 'smtp.zoho.com';      // Consult your hosting provider.
+$outgoingServerPort         = '465';                // Options: '587' , '25' - Consult your hosting provider.
+$outgoingServerSecurity     = 'ssl';                // Options: 'ssl' , 'tls' , null - Consult your hosting provider.
 
 // Sending Account Settings - replace these details with an email account held on the SMTP server entered above.
 // These 2 settings are only required if you choose 'smtp' for emailMethod above.
-$sendingAccountUsername     = 'email@domain.com';
-$sendingAccountPassword     = 'p@55w0rd';
+$sendingAccountUsername     = 'webmaster@ovva.sk';
+$sendingAccountPassword     = 'G761yjEfbkdQ';
 
 // Recipient (To:) Details  - Change this to the email details of who will receive all the emails from the website.
-$recipientEmail             = 'email@yourdomain.com'; // REQUIRED value.
-$recipientName              = 'John Doe';             // REQUIRED value.
+$recipientEmail             = 'info@ovva.sk'; // REQUIRED value.
+$recipientName              = 'Revael OVVA.sk'; // REQUIRED value.
 
 // Email details            - Change these to suit your website needs
-$emailSubject               = 'A message from Your Website'; // REQUIRED value. Subject of the email that the recipient will see
-$websiteName                = 'Your Website';                // REQUIRED value. This is used when a name or email is not collected from the website form
+$emailSubject               = 'A message from REVEAL.eu Website'; // REQUIRED value. Subject of the email that the recipient will see
+$websiteName                = 'Reveal App';                // REQUIRED value. This is used when a name or email is not collected from the website form
 
-$timeZone                   = 'Australia/Melbourne';         // OPTIONAL, but some servers require this to be set. 
+$timeZone                   = 'Europe/Bratislava';           // OPTIONAL, but some servers require this to be set. 
                                                              // See a list of all supported timezones at: http://php.net/manual/en/timezones.php
 
 // Google reCAPTCHA
@@ -36,10 +36,10 @@ $recaptchaErrorMessage      = 'There was a problem verifying the Google reCaptch
 
 // Confirmation Message
 $sendConfirmationToUser     = false; // leave false to disable confirmation, or use path to an html email template.
-$confirmationSubject        = "Thanks for Contacting Company Name"; // The subject of the confirmation email
-$confirmationFromName       = "Company Name"; // Used in the "from" field of the email.
+$confirmationSubject        = "Thanks for Contacting Reveal"; // The subject of the confirmation email
+$confirmationFromName       = "Reveal App"; // Used in the "from" field of the email.
 $userEmailField             = "email";
-$defaultUserName            = "User"; // Used if the form does not include a "name" field.
+$defaultUserName            = "webmaster"; // Used if the form does not include a "name" field.
 
 // CSV
 $saveToCSV                  = "email_form.csv";
